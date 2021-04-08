020779b225e81af4e23f0b7cc6214e5662d39be3
Julian Schwazer <hello@julianschwazer.at>
2021-04-08 16:58:29 +0200

    adding safety platforms and other stuff

---
+++ Assets/GueneyJulianBaskiceball/Scripts/platform_movement.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform_movement : MonoBehaviour
+{
+    public GameObject Player; // reference to the player
+    
+    private int waypoint_active = 0; // number of available waypoints for the editor and default value
+    
+    private float tolerance; // tolerance for the movement
+    public float speed; // speed for the movement
+    public float delay; // delay before platform moves to the next waypoint
+    private float delay_timer; // delay timer 
+    public bool automatic; // boolean for automatic start of the platform movement
+    
+    public Vector3[] waypoints; // array for the waypoints
+    private Vector3 target_position; // variable for target position of platform movement
+
+    [Header("Platform Scaling")]
+    public bool scaleOnEnter; // enable scale on enter
+    public bool scaleOnLeave; // enable scale on leave
+    
+    public float scaleX; // variable for scaling
+    public float scaleY; // variable for scaling
+    public float scaleZ; // variable for scaling
+
+    void Start()
+    {
+        //convert waypoint list to target positions
+        if (waypoints.Length > 0)
+        {
+            target_position = waypoints[0];
+        }
+
+        tolerance = speed * Time.deltaTime; //set a tolerance
+    }
+
+    void Update()
+    {
+        if (transform.position != target_position)
+        {
+            MovePlatform(); // include movement function
+        }
+        else
+        {
+            UpdateTarget(); // include update target function
+        }
+    }
+
+    // movement of the platform
+    void MovePlatform()
+    {
+        Vector3 heading = target_position - transform.position;
+        transform.position += (heading / heading.magnitude) * speed * Time.deltaTime;
+        
+        if (heading.magnitude < tolerance)
+        {
+            transform.position = target_position;
+            delay_timer = Time.time;
+        }
+    }
+
+    // automatic start of platform and delay
+    void UpdateTarget()
+    {
+        if (automatic)
+        {
+            if (Time.time - delay_timer > delay)
+            {
+                NextPlatform();
+            }
+        }
+    }
+
+    // setting the next waypoint for the platform movement + reset to first waypoint
+    void NextPlatform()
+    {
+        waypoint_active++;
+        if (waypoint_active >= waypoints.Length)
+        {
+            waypoint_active = 0;
+        }
+
+        target_position = waypoints[waypoint_active];
+    }
+
+    // parent player to platform - player moves with the platform
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+            if (scaleOnEnter == true)
+                this.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+
+        Player.transform.parent = transform;
+    }
+
+    // player separates from platform when jumping
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+
+            if (scaleOnLeave == true)
+                this.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+        }
+
+    }
+}
--- Assets/GueneyJulianBaskiceball/Scripts/player_script.cs
+++ Assets/GueneyJulianBaskiceball/Scripts/player_script.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 
 public class player_script : MonoBehaviour
 {
+    // Variables and References
+    [Header("Player Movement Settings")]
+    public float mvmt_speed = 10f; // movement speed variable for editor with default value
+    public float jump_height = 10f; // jump height of the player
     
-    public float speed = 10f; // movement speed variable for editor with default value
-    public float jumpheight = 10f; // jump height of the player
-    public Rigidbody rb; // initialize rigidbody 
+    private Rigidbody rb; // initialize rigidbody 
+    
     private bool ballIsJumpable = true; // boolean to only jump the ball is jumpable
-    
-    public GameObject Floor;
+    public bool horizontalMovement = false; // enable horizontal ball movement in the editor
     
     // Start is called before the first frame update
     void Start()
@@ -22,21 +24,24 @@ public class player_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         // Horizontal Movement - Left/Right with Arrow Keys
-        //float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        //transform.Translate(horizontal, 0, 0);
+        if (horizontalMovement)
+        {
+            float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * mvmt_speed;
+            transform.Translate(horizontal, 0, 0);
+        }
+        
 
         // Jump Movement - Up with Spacebar, just works when Ball is on the floor
         if (Input.GetButtonDown("Jump") && ballIsJumpable)
-        {
-            rb.AddForce(new Vector3(0, jumpheight,0), ForceMode.Impulse);
-            ballIsJumpable = false;
-        }
+            {
+                rb.AddForce(new Vector3(0, jump_height,0), ForceMode.Impulse);
+                ballIsJumpable = false;
+            }
 
     }
     
-    // Set back the variable to allow another jump
+    // Set back the variable to allow another jump when the ball hits a platform
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Jumpable") 
