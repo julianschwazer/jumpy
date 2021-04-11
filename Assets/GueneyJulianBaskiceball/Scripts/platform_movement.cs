a60b782cedcd954775f8e622e21dffd2f694f79a
guneyguner <guner444@gmail.com>
2021-04-11 22:51:56 +0200

    new extensions

--- Assets/GueneyJulianBaskiceball/Scripts/platform_movement.cs
+++ Assets/GueneyJulianBaskiceball/Scripts/platform_movement.cs
@@ -27,21 +27,21 @@ public class platform_movement : MonoBehaviour
     public float scaleY; // variable for scaling
     public float scaleZ; // variable for scaling
 
-    private float initialX; //initial X scale
-    private float initialY; //initial Y scale
-    private float initialZ; //initial Z scale
+    private float initialX; // initial X scale
+    private float initialY; // initial Y scale
+    private float initialZ; // initial Z scale
     
-    private float currentScale = 0; // current scale of the platform
-    private float targetScale = 100; // target scale of the platform
+    private float currentScale = 0; // start for percentage calculation
+    private float targetScale = 100; // end for percentage calculation
     private const int FramesCount = 200; // frame count = scaling speed
     private float ds; // delta scale
     private const float AnimationTimeSeconds = 2; // scaling time in s
     private float deltaTime = AnimationTimeSeconds/FramesCount; // scaling
    
-    private float tileX = 5; 
-    private float tileY = 5;
-    private float initTileX = 1;
-    private float initTileY = 1;
+    private float tileX = 5; // target X variable of tile scale
+    private float tileY = 5; // target Y variable of tile scale
+    private float initTileX = 1; // initial X variable of tile scale
+    private float initTileY = 1; // initial Y variable of tile scale
 
     void Start()
     {
@@ -51,13 +51,13 @@ public class platform_movement : MonoBehaviour
         initialY = transform.localScale.y;
         initialZ = transform.localScale.z;
             
-        //convert waypoint list to target positions
+        // convert waypoint list to target positions
         if (waypoints.Length > 0)
         {
             target_position = waypoints[0];
         }
 
-        tolerance = speed * Time.deltaTime; //set a tolerance
+        tolerance = speed * Time.deltaTime; // set a tolerance
     }
 
     void Update()
@@ -148,19 +148,19 @@ public class platform_movement : MonoBehaviour
                 currentScale = targetScale;
             }
 
-            float currentXscale = (scaleX - initialX) / 100 * currentScale + initialX; //X scale percentage 
-            float currentYscale = (scaleY - initialY) / 100 * currentScale + initialY; //Y scale percentage
-            float currentZscale = (scaleZ - initialZ) / 100 * currentScale + initialZ; //Z scale percentage
+            float currentXscale = (scaleX - initialX) / 100 * currentScale + initialX; // X scale percentage 
+            float currentYscale = (scaleY - initialY) / 100 * currentScale + initialY; // Y scale percentage
+            float currentZscale = (scaleZ - initialZ) / 100 * currentScale + initialZ; // Z scale percentage
            
-            float currentTileX = (tileX - initTileX) / 100 * currentScale + initTileX; //tile X scale percentage 
-            float currentTileY = (tileY - initTileY) / 100 * currentScale + initTileY; //tile Y scale percentage
+            float currentTileX = (tileX - initTileX) / 100 * currentScale + initTileX; // tile X scale percentage 
+            float currentTileY = (tileY - initTileY) / 100 * currentScale + initTileY; // tile Y scale percentage
             
-            GetComponent<Renderer>().material.mainTextureScale = new Vector2(currentTileX, currentTileY); //update the tile scale
+            GetComponent<Renderer>().material.mainTextureScale = new Vector2(currentTileX, currentTileY); // update the tile scale
 
             var prevParent = Player.transform.parent; // remember the previous parent
             
             Player.transform.parent = null; // so that player doesn't also scale with platform
-            transform.localScale = new Vector3(currentXscale,currentYscale,currentZscale); //update the scale
+            transform.localScale = new Vector3(currentXscale,currentYscale,currentZscale); // update the scale
             Player.transform.parent = prevParent; // reattach the player to the previous parrent
             
             yield return new WaitForSeconds(deltaTime);
