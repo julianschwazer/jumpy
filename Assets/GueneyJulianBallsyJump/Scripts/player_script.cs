31a736339c7651e8e30a7e8c5adce787a2334349
Julian Schwazer <59028662+julianschwazer@users.noreply.github.com>
2021-11-18 16:25:16 +0100

    updating unity and changing the game mechanics to jump and fly

--- Assets/GueneyJulianBallsyJump/Scripts/player_script.cs
+++ Assets/GueneyJulianBallsyJump/Scripts/player_script.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 
 public class player_script : MonoBehaviour
 {
+    
     // Variables and References
-    [Header("Player Movement Settings")]
-    public float mvmt_speed = 10f; // movement speed variable for editor with default value
-    public float jump_height = 10f; // jump height of the player
-    
     private Rigidbody rb; // initialize rigidbody 
-    
-    private bool ballIsJumpable = true; // boolean to only jump the ball is jumpable
-    public bool horizontalMovement = false; // enable horizontal ball movement in the editor
+    private bool birdIsJumpable = true; // boolean to enable/disable the jumping
+    private bool birdIsFlyable = false; // boolean to enable/disable the flying
+    private bool birdIsFlapable = false;
+
+    // PUBLIC Variables for the editor
+    [Header("Player Jumping Settings")]
+    public float jump_speed = 10f; // movement speed variable for editor with default value
+    public float jump_height = 10f; // jump height of the player
+
+    [Header("Player Flying Settings")] 
+    public float fly_horizontalspeed = 10f;
+    public float fly_height = 2f;
+    public float fly_flaptime = 2f;
     
     // Start
     void Start()
@@ -24,47 +31,92 @@ public class player_script : MonoBehaviour
     // Update
     void Update()
     {
-        // Horizontal Movement - Left/Right with Arrow Keys
-        if (horizontalMovement)
+        // Vertical Movement - Jumping with all "Up-Keys", SpaceBar, and MouseButton
+        if (birdIsJumpable)
         {
-            float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * mvmt_speed;
-            transform.Translate(horizontal, 0, 0);
+            if (Input.GetButtonDown("Jump") && birdIsJumpable
+                || Input.GetMouseButtonDown(0) && birdIsJumpable
+                || Input.GetKeyDown(KeyCode.UpArrow) && birdIsJumpable
+                || Input.GetKeyDown(KeyCode.W) && birdIsJumpable)
+            {
+                rb.AddForce(new Vector3(0, jump_height,0), ForceMode.Impulse);
+                birdIsJumpable = false;
+            }
         }
-        
 
-        // Jump Movement – Allowing the Ball to jump with all "Up-Keys", SpaceBar and MouseButton
-        if (Input.GetButtonDown("Jump") && ballIsJumpable
-            || Input.GetMouseButtonDown(0) && ballIsJumpable
-            || Input.GetKeyDown(KeyCode.UpArrow) && ballIsJumpable
-            || Input.GetKeyDown(KeyCode.W) && ballIsJumpable)
+        // Vertical Movement – Flying/flapping the wings with all "Up-Keys", SpaceBar, and MouseButton
+        if (birdIsFlyable)
         {
-            rb.AddForce(new Vector3(0, jump_height,0), ForceMode.Impulse);
-            ballIsJumpable = false;
+            // Horizontal Movement
+            float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * fly_horizontalspeed;
+            rb.AddForce(new Vector3(horizontal, 0,0), ForceMode.Impulse);
+
+            // Vertical Movement
+            if (Input.GetButtonDown("Jump") && birdIsFlapable
+                || Input.GetMouseButtonDown(0) && birdIsFlapable
+                || Input.GetKeyDown(KeyCode.UpArrow) && birdIsFlapable
+                || Input.GetKeyDown(KeyCode.W) && birdIsFlapable)
+            {
+                rb.AddForce(new Vector3(0, fly_height,0), ForceMode.Impulse);
+                StartCoroutine(DisableFlapping());
+            }
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         // Set back the variable to allow another jump when the ball hits a platform
-        if (collision.gameObject.tag == "Jumpable") 
+        if (collision.gameObject.CompareTag("Jumpable")) 
         {
-            ballIsJumpable = true;
+            if (birdIsFlyable == false)
+            {
+                birdIsJumpable = true;
+            }
         }
         
+        
         // Adding Force to the enemy so that they are pushing the player away
-        if (collision.gameObject.tag == "Enemy") 
+        if (collision.gameObject.CompareTag("Enemy")) 
         {
             rb.AddForce(200,200,200,ForceMode.Force);
         }
         
+        
         // Adding Force to the walls so that the player is not able to glitch through them
-        if (collision.gameObject.tag == "WallLeft") 
+        if (collision.gameObject.CompareTag("WallLeft")) 
         {
             rb.AddForce(200,0,0,ForceMode.Force);
         }
-        if (collision.gameObject.tag == "WallRight") 
+        if (collision.gameObject.CompareTag("WallRight")) 
         {
             rb.AddForce(-200,0,0,ForceMode.Force);
         }
     }
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        // Enable/Disable the flying capabilities when jumping through the right object
+        if (trigger.gameObject.CompareTag("FlyingAreaEnter")) 
+        {
+            birdIsJumpable = false;
+            birdIsFlyable = true;
+            birdIsFlapable = true;
+            Debug.Log("I am flying");
+        }
+        if (trigger.gameObject.CompareTag("FlyingAreaLeave"))
+        {
+            birdIsJumpable = true;
+            birdIsFlyable = false;
+            Debug.Log("I am leaving the fly area");
+        }
+    }
+    
+    private IEnumerator DisableFlapping() {
+        birdIsFlapable = false;
+ 
+        yield return new WaitForSeconds(fly_flaptime);
+
+        birdIsFlapable = true;
+    }
+    
 }
