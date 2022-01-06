e265bd0b782f5a6bb444559f4a3e4bf648cc927f
Julian Schwazer <59028662+julianschwazer@users.noreply.github.com>
2022-01-06 17:07:36 +0100

    project cleanup + game manager + audio manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void EndGame()
+    {
+#if UNITY_EDITOR // end game - only in editor
+        EditorApplication.ExitPlaymode();
+#endif
+
+        Application.Quit();
+    }
+}
